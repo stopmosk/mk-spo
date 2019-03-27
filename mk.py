892e6124a38ffb0e858d817343f3d01285953f2f
Sergei Shutov <48527749+stopmosk@users.noreply.github.com>
2019-03-27 12:58:13 +0300

    Upd

--- mk.py
+++ mk.py
@@ -8,24 +8,23 @@ db = google_db_read()
 mk_list = [['МК Сиьска', 1],  # Название и вместимость МК
            ['МК Жопа', 2],
            ['МК Лох', 2],
-           ['МК Ублюдок, мать твою', 4],
-           ['МК Заебись', 3],
+           ['МК Ублюдок, мать твою', 5],
+           ['МК Заебись', 6],
            ['Пустой', 2],
            ['Пустой2', 2]]
 
 sorted_list = sort_mk_list(db, mk_list)   # Отсортировали МК по популярности
 
 print('***************** РАСПРЕДЕЛЕНИЕ ПО МК ***************************')
-mk_result, satisfaction = assign_people_v2(db, mk_list, pr)
+mk_result, loosers, satisfaction = assign_people_v2(db, mk_list, pr)
 for mk in mk_result:
     print(mk[0])
     for name in mk[2]:
         print('\t', name)
 
 print('*************** ЛЮДИ, ОСТАВШИЕСЯ НЕРАСПРЕДЕЛЁННЫМИ ****************')
-for name in db:
-    if db[name].count(' ') == 0:
-        print(name)
+for name in loosers:
+    print(name)
 
 print('Процент удовлетворённости распределением: ' +
       str(int(satisfaction * 100)) + '%')
--- mk_logic.py
+++ mk_logic.py
@@ -1,3 +1,45 @@
+def assign_people_v2(db, mk_list, max_priority):
+    # Функция распределения людей по МК.
+    # Внешний цикл - приоритет, внутренний - МК
+    result = []
+    satisf = 0   # Удовлетворённость распределением
+    names = set(db.keys())
+    for mk in mk_list:
+        result.append([mk[0], mk[1], []])
+    for priority in range(max_priority):
+        for i, mk in enumerate(mk_list):
+            if result[i][1] == 0:   # Если свободных мест в данном МК уже нет
+                continue
+            for name in db:
+                if db[name][priority] == mk[0]:
+                    if name not in names:
+                        continue
+                    s_pers = int((1 - priority/max_priority) * 100)
+                    result[i][2].append(name + ', ' + str(s_pers))
+                    satisf += max_priority - priority   # Удовлетворённость
+                    names.remove(name)
+                    result[i][1] -= 1   # Уменьшить кол-во свободных мест
+                if result[i][1] == 0:
+                    break
+
+    print(result)
+    return result, names, satisf / len(db) / max_priority
+
+
+def sort_mk_list(db, mk_list):
+    result = []
+    for mk in mk_list:
+        cnt = 0
+        for name in db:
+            for p, sel in enumerate(db[name]):
+                if sel == mk[0]:
+                    cnt += 1
+        result.append([mk[0], mk[1], cnt / mk[1]])
+    # result.sort(key=lambda res: res[2])
+    result.sort(key=lambda res: res[2], reverse=True)
+    return result
+
+
 def assign_people(db, mk_list, max_priority):
     # Функция распределения людей по МК
     # Внешний цикл - МК, внутренний - приоритет.
@@ -24,44 +66,3 @@ def assign_people(db, mk_list, max_priority):
         result.append([mk[0], free_slots, stu_list])
     return result, satisf / len(db) / max_priority
 
-
-def assign_people_v2(db, mk_list, max_priority):
-    # Функция распределения людей по МК.
-    # Внешний цикл - приоритет, внутренний - МК
-
-    result = []
-    satisf = 0   # Удовлетворённость распределением
-    for mk in mk_list:
-        result.append([mk[0], mk[1], []])
-    for priority in range(max_priority):
-        for i, mk in enumerate(mk_list):
-            if result[i][1] == 0:   # Если свободных мест в данном МК уже нет
-                continue
-            for name in db:
-                if db[name][priority] == mk[0]:
-                    result[i][2].append(name)
-                    satisf += max_priority - priority   # Удовлетворённость
-                    db[name] = list(' ' * max_priority)
-                    # Я негодяй и меняю аргументы функции. Знаю, плохо
-                    result[i][1] -= 1   # Уменьшить кол-во свободных мест
-                if result[i][1] == 0:
-                    break
-
-        # Теперь забьём оставшиеся пустые места МК прочерками
-        '''for f_slot in range(free_slots, 0, -1):
-            stu_list.append('---')'''
-    return result, satisf / len(db) / max_priority
-
-
-def sort_mk_list(db, mk_list):
-    result = []
-    for mk in mk_list:
-        cnt = 0
-        for name in db:
-            for p, sel in enumerate(db[name]):
-                if sel == mk[0]:
-                    cnt += 1
-        result.append([mk[0], mk[1], cnt / mk[1]])
-    # result.sort(key=lambda res: res[2])
-    result.sort(key=lambda res: res[2], reverse=True)
-    return result
