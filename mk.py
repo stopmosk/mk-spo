c5b06df650df361737645742b5c00b9cc93dda9b
Sergei Shutov <48527749+stopmosk@users.noreply.github.com>
2019-03-27 11:50:42 +0300

    Улучшил алгоритм (кажется)

--- mk.py
+++ mk.py
@@ -5,21 +5,21 @@ from mk_google_spreadsheets import *
 pr = 4   # Длина списка приоритетов
 
 db = google_db_read()
-mk_list = [['МК Сиьска', 3],  # Название и вместимость МК
-           ['МК Жопа', 2],
+mk_list = [['МК Сиьска', 4],  # Название и вместимость МК
+           ['МК Жопа', 3],
            ['МК Лох', 1],
-           ['МК Ублюдок, мать твою', 2],
-           ['МК Заебись', 2],
+           ['МК Ублюдок, мать твою', 4],
+           ['МК Заебись', 3],
            ['Пустой', 2],
            ['Пустой2', 2]]
 
 sorted_list = sort_mk_list(db, mk_list)   # Отсортировали МК по популярности
 
 print('***************** РАСПРЕДЕЛЕНИЕ ПО МК ***************************')
-mk_result = assign_people(db, sorted_list, pr)
+mk_result = assign_people_v2(db, sorted_list, pr)
 for mk in mk_result:
     print(mk[0])
-    for name in mk[1]:
+    for name in mk[2]:
         print('\t', name)
 
 print('*************** ЛЮДИ, ОСТАВШИЕСЯ НЕРАСПРЕДЕЛЁННЫМИ ****************')
--- mk_logic.py
+++ mk_logic.py
@@ -1,5 +1,6 @@
 def assign_people(db, mk_list, max_priority):
-    # Список МК сортирован от малопопулярных к популярным?
+    # Функция распределения людей по МК
+    # Внешний цикл - МК, внутренний - приоритет.
     result = []
     for mk in mk_list:
         stu_list = []   # Список людей, которых запишут на данный МК
@@ -18,7 +19,40 @@ def assign_people(db, mk_list, max_priority):
         # Теперь забьём оставшиеся пустые места МК прочерками
         for f_slot in range(free_slots, 0, -1):
             stu_list.append('---')
-        result.append([mk[0], stu_list])
+        result.append([mk[0], free_slots, stu_list])
+    return result
+
+
+def assign_people_v2(db, mk_list, max_priority):
+    # Функция распределения людей по МК.
+    # Внешний цикл - приоритет, внутренний - МК
+
+    result = []
+    for mk in mk_list:
+        result.append([mk[0], mk[1], []])
+    print(result)
+
+    for priority in range(max_priority):
+        print(priority)
+        print(mk_list)
+        for i, mk in enumerate(mk_list):
+            print('\t', i, mk[0], result[i][1])
+            if result[i][1] == 0:   # Если свободных мест в данном МК уже нет
+                continue
+            for name in db:
+                if db[name][priority] == mk[0]:
+                    result[i][2].append(name)
+                    db[name] = list(' ' * max_priority)
+                    print('\t\t\t', name)
+                    # Я негодяй и меняю аргументы функции. Знаю, плохо
+                    result[i][1] -= 1   # Уменьшить кол-во свободных мест
+                if result[i][1] == 0:
+                    break
+
+        # Теперь забьём оставшиеся пустые места МК прочерками
+        '''for f_slot in range(free_slots, 0, -1):
+            stu_list.append('---')'''
+
     return result
 
 
@@ -31,5 +65,6 @@ def sort_mk_list(db, mk_list):
                 if sel == mk[0]:
                     cnt += 1
         result.append([mk[0], mk[1], cnt / mk[1]])
+    # result.sort(key=lambda res: res[2])
     result.sort(key=lambda res: res[2], reverse=True)
     return result
